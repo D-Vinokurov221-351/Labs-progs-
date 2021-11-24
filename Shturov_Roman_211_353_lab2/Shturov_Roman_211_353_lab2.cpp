e365e2eecc0ff6326de5ed228197aca2cdadeb6b
SirAlbaToR <90750816+SirAlbaToR@users.noreply.github.com>
2021-11-24 10:25:13 +0300

    test

---
+++ Shturov_Roman_211_353_lab2/Shturov_Roman_211_353_lab2.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int trio()
+{    
+    int i;
+    cin >> i, "\n";
+    int c = 0;
+    int count = 1;
+    int r = 0;
+    while( r != i+1)
+        if (c != count)
+        {
+            cout << c;
+            c+=1;
+        }
+        else
+        {
+            cout << "\n";
+            r = c;
+            count += 1;
+            c = 0;
+        }
+
+    return 0;
+}
+int cord()
+{
+    double coord;        
+    cin >> coord;
+    double count = 0, count1 = 0;
+    while (coord != 0)
+        if (coord != 0)
+        {
+            count += 1;
+            count1 += coord;
+            cin >> coord;
+        }
+
+    cout << count1 / count << endl;
+    return 0;
+}
+int Binom()
+{
+    int n, k;
+    cin >> n;
+    cin >> k;
+    double res = 1;
+    for (int i = 1; i <= k; ++i)
+        res = res * (n - k + i) / i;
+    cout << res + 0.01 << endl;
+    return 0;
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int choise;
+    while (true)
+    {
+        std::cout << "Что вы хотите выполнить?" "\n" << "1. задание 1" "\n" << "2. задание 2" "\n" << "3. задание 3" "\n" << "4. выход" "\n";
+        cin >> choise;
+        switch (choise)
+        {
+        case 1:
+            trio();
+            break;
+        case 2:
+            Binom();
+            break;
+        case 3:
+            cord();
+            break;
+        case 4:
+            cout << "Всего хорошего";
+
+        default:
+            break;
+        }
+    }
+}
---
+++ Shturov_Roman_211_353_lab32/Shturov_Roman_211_353_lab32.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+void task1(int *arr, int arr_size) {
+    cout << "Введите массив ";
+    for (int i = 0; i < arr_size; i++) {
+        cin >> arr[i];
+    }
+}
+
+void task2(int *arr, int arr_size) {
+    for (int i = 0; i < arr_size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+void task3() {
+
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int choise;
+    int arr_size;
+    cout << "Введите длину массива "; cin >> arr_size;
+    int *arr = new int[arr_size];
+    while (true)
+    {
+        std::cout << "Что вы хотите выполнить?" "\n" << "1. задание 1" "\n" << "2. задание 2" "\n" << "3. задание 3" "\n" << "4. выход" "\n";
+        cin >> choise;
+        
+        switch (choise)
+        {
+        case 1:
+            task1(arr,arr_size);
+            break;
+         case 2:
+            task2(arr, arr_size);
+            break;
+        case 3:
+            task3();
+            break;
+        case 4:
+            cout << "Всего хорошего";
+         default:
+            break;
+        }
+    }
+    delete[] arr;
+    return 0;
+}
