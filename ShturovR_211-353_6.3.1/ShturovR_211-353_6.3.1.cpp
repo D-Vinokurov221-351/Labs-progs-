9c38ebaeac9b052051a3a43b6f6447454b7a7614
SirAlbaToR <90750816+SirAlbaToR@users.noreply.github.com>
2022-02-04 19:35:09 +0300

    04.02.2022

--- ShturovR_211-353_6.3.1/ShturovR_211-353_6.3.1.cpp
+++ ShturovR_211-353_6.3.1/ShturovR_211-353_6.3.1.cpp
@@ -5,26 +5,31 @@
 using std::cin;
 using std::cout;
 
-long long Div(long long a) {
-    while (a % 2 == 0)
-        a /= 2;
-    while (a % 3 == 0)
-        a /= 3;
-    while (a % 5 == 0)
-        a /= 5;
+long long Div(long long a){
+    bool flag = 0;
+    long long b = a;
+    int abc[8] = { 7,11,13,17,19,21,27,29 };
+    do { b /= 30; } while (b / 30 > 30);
+    for (int i = 0; i < 8; i++) {
+        if (abc[i] == b) {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 1) return 0;
     return a;
 }
 
 int main() {
-    long long a = 2, count = 0, n;
+    long long a = 30, count = 0, n;
     std::queue <int> out;
     cin >> n;
     while (count < n) {
-        if (Div(a) == 1) {
+        if (Div(a) != 0) {
             out.push(a);
             ++count;
         }
-        ++a;
+        a+=30;
     }
 
     for (int i = 0; i < count; i++) {
